38218d6f824b2652fafcb137430101fc864a1f4b
Yuhki <missbsd@gmail.com>
2024-05-08 12:23:41 +0900

    수정사항 작업중

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"runtime"
 	"zkeep/config"
+	"zkeep/global"
 	"zkeep/models"
 	"zkeep/services"
 
@@ -21,6 +22,59 @@ func main() {
 	log.Info("Server Start")
 
 	models.InitCache()
+
+	conn := models.NewConnection()
+
+	buildingManager := models.NewBuildingManager(conn)
+	facilityManager := models.NewFacilityManager(conn)
+
+	buildings := buildingManager.Find(nil)
+
+	for _, b := range buildings {
+		id := b.Id
+		items := facilityManager.Find([]interface{}{
+			models.Where{Column: "building", Value: id, Compare: "="},
+		})
+
+		var total float64 = 0
+		for _, v := range items {
+			if v.Category == 10 {
+				total += float64(global.Atoi(v.Value2))
+			}
+
+			if v.Category == 20 {
+				total += float64(global.Atoi(v.Value12))
+			}
+
+			if v.Category == 30 {
+				total += float64(global.Atoi(v.Value6))
+			}
+
+			if v.Category == 40 {
+				total += float64(global.Atoi(v.Value5))
+			}
+
+			if v.Category == 50 {
+				total += float64(global.Atoi(v.Value4))
+			}
+
+			if v.Category == 60 {
+				total += float64(global.Atoi(v.Value12))
+			}
+
+			if v.Category == 70 {
+				total += float64(global.Atoi(v.Value4))
+			}
+
+			if v.Category == 90 {
+				total += float64(global.Atoi(v.Value6))
+			}
+		}
+
+		buildingManager.UpdateTotalweight(models.Double(total), id)
+	}
+
+	conn.Close()
 
 	services.Cron()
 	services.Chat()
